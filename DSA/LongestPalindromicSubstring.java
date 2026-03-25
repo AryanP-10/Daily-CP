f71b32df9a44edc9e3c360e786cd277a06a0a947
AryanP-10 <aryanparadkar10@gmail.com>
2026-03-25 21:28:55 +0530

    Added: Longest Palindromic Substring (LeetCode #5) - Medium - Strings, Two Pointers

---
+++ DSA/LongestPalindromicSubstring.java
@@ -0,0 +1,31 @@
+// Problem: Longest Palindromic Substring
+// Platform: LeetCode #5
+// Difficulty: Medium
+// Link: https://leetcode.com/problems/longest-palindromic-substring/
+// Topic: Strings, Two Pointers
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
+class Solution {
+    public String longestPalindrome(String s) {
+        String res = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            String odd = expand(s, i, i);       // odd length
+            String even = expand(s, i, i + 1);  // even length
+
+            if (odd.length() > res.length()) res = odd;
+            if (even.length() > res.length()) res = even;
+        }
+
+        return res;
+    }
+
+    private String expand(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        return s.substring(l + 1, r);
+    }
+}
